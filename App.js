079b702b5c17716103bcd2141207c35d61426464
im-agarcia <im.agarcia@otlook.com>
2021-05-18 20:47:21 -0300

    Primera subida

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+  View, 
+  Text, 
+  StyleSheet, 
+  AppRegistry,
+  Button
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  export default class MovieHelper extends Component{
+    constructor(){
+      super()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      this.state ={
+        status: false,
+        data: null
+      }
+    }
+
+
+    render(){
+      return(
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Movie Helper!
+          </Text>
+          <Text>
+
+          </Text>
+          <Text>
+
+          </Text>
+        </View>
+      )
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'azure'     
+    },
+    welcome:{
+      fontSize: 20,
+      textAlign: 'center',
+      color: 'blue',
+
+    }
+  })
