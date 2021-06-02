bf350f02a2c7550d2dd7beb387579893aba529c5
LDibiase <tamaraberrocal@gmail.com>
2021-06-02 18:31:32 -0300

    Login and refactor

--- App.js
+++ App.js
@@ -1,44 +1,60 @@
 import React from 'react';
-import {View, Text, TextInput, StyleSheet, AppRegistry, Button} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  AppRegistry,
+  Button,
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
-import Home from './pages/home/index'
-import Listado from "./components/listado";
+import { Home, Listado, Login, SignUp } from './pages';
 
 const Stack = createStackNavigator();
 
-  export default function App() {
-          return(
-            <NavigationContainer>
-              <Stack.Navigator initialRouteName={'Login'}>
-                <Stack.Screen name= 'Home' component= {Home}/>
-                <Stack.Screen name= 'Listado' component= {Listado}/>
-                <Stack.Screen name= 'Login' component= {Login}/>
-              </Stack.Navigator>
-            </NavigationContainer>
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={'Login'}>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen name="Listado" component={Listado} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: 'Login' }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ title: 'Crear cuenta' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-      )
-    };
-
-
-  const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'azure'     
-    },
-    welcome:{
-      fontSize: 20,
-      textAlign: 'center',
-      color: 'blue',
-
-    },
-    textInput:{
-      height: 40,
-      border: 'solid grey',
-      borderRadius: 8
-    }
-  })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'azure',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'blue',
+  },
+  textInput: {
+    height: 40,
+    borderStyle: 'solid',
+    borderColor: 'grey',
+    borderRadius: 8,
+  },
+});
